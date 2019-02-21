18db384e2976ba95ca128ebeff8f7be918bec740
Brian Marquis <rbmarquis.dev@gmail.com>
2019-02-20 19:15:38 -0600

    implement db connection with serialization

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport'); 
 const session = require('express-session');
+const ObjectID = require('mongodb').ObjectID;
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -27,12 +29,39 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongo.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, db) => {
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+    //serialization and app.listen 
+
+    // Setup user object serialization & deserialization
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+      db.collection('users').findOne(
+        {_id: new ObjectID(id)},
+        (err, doc) => {
+          done(null, doc);
+        }
+      );
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+
+  }
+});
+
+
 app.route('/')
   .get((req, res) => {
     // render view template and send template variable values
     res.render('pug/index', {title: 'Hello', message: 'Please login'});
   });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+
